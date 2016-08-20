6ccedbe65bd3ed51191a2638938140098748970b
danielarnost <danielarnost@utexaas.edu>
2016-08-20 13:17:20 -0500

    HTML

---
+++ assets/javascript/game.js
@@ -0,0 +1,84 @@
+
+ ;$(document).ready(function(){
+    alert( "welcome" );
+ 
+});
+//alert ("hello");// test to see if linked properly
+//GLOBAL VARIABLES
+//=====================================================
+//Arrays and variables for holding data
+$( ".crystal" ).click(function() {
+   .b
+});
+
+//var computerAnswer = []
+
+
+var computerAnswer = []
+var winCounter = 0
+var lossCounter = 0 
+var userGameCounter = []
+var crystalValue = []
+
+
+// sub category game counters (Global)
+//__________________________________________________________
+
+
+//Functions (Reusable blocks of code )
+$(document).ready(function(){
+
+//===============================================
+//*generate computer guess
+var computerAnswer = (Math.floor(Math.random()*100))
+$Math.random(computerAnswer)
+
+
+//reset functions - every time it runs start game these need to be reset
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+//populate DOM====================================================
+// use for loops for to check if user addition = computerGuess
+
+
+
+
+
+//var x = Y+w
+//varY=5
+//document.getElementbyID"demo
+
+
+
+
+
+
+
+
+
+
+//TENOR use on click function for crystals
+$( ".crystal" ).click(function() {
+
+});
+
+//check if user won : if statement
+
+
+////add hidden value
+
+
+///add addition function (calculator)
+
+
+
+
+
+
+//Main processes, call functions , 
+//====================================================
+
+//Initiaiates code for first time
+$startGame();
